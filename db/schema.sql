b884853b5f3d43a8022dcf65dabbc0c38ab07eab
justbouton <justb1985@gmail.com>
2019-01-17 21:06:04 -0800

    Create schema.sql with the following tables: users, trips, dates, locations, activities. Created seeds.sql with data. Tested table joins.

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,37 @@
--- create database
-DROP DATABASE IF EXISTS triplan_db;
-CREATE DATABASE triplan_db;
+DROP database if exists triplan_db;
+
+CREATE database triplan_db;
+
 USE triplan_db;
 
--- create user table
 CREATE TABLE users(
-    user_id INT NOT NULL AUTO_INCREMENT,
-    username VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    PRIMARY KEY (user_id)
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(60) not null,
+    email NVARCHAR(45) not null UNIQUE,
+    password_hash NVARCHAR(16) not null
 );
 
+CREATE TABLE trips(
+    trip_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    trip_name NVARCHAR(45) not null,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE dates (
+    date_id DATE PRIMARY KEY,
+    trip_id INT not null,
+    FOREIGN KEY (trip_id) REFERENCES trips(trip_id)  
+);
+
+CREATE TABLE locations (
+    location_name VARCHAR(60) PRIMARY KEY,
+    date_id DATE,
+    FOREIGN KEY (date_id) REFERENCES dates(date_id)  
+);
+
+CREATE TABLE activities (
+    activity_name VARCHAR(255) not null PRIMARY KEY,
+    location_name VARCHAR(60),
+    FOREIGN KEY (location_name) REFERENCES locations(location_name)  
+);
